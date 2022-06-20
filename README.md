b03f6256c124695638a0a21881f6c83a1a09d6b8
Rodney Lab <codesign@rodneylab.com>
2022-06-20 19:56:08 +0100

    ci: 🐝 initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+<img src="./images/rodneylab-github-svelte-chartjs.png" alt="Rodney Lab svelte-chartjs Github banner">
+
+<p align="center">
+  <a aria-label="Open Rodney Lab site" href="https://rodneylab.com" rel="nofollow noopener noreferrer">
+    <img alt="Rodney Lab logo" src="https://rodneylab.com/assets/icon.png" width="60" />
+  </a>
+</p>
+<h1 align="center">
+  Svelte Chart.js
+</h1>
+
+# svelte-chart.js
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/rodneylab/svelte-chartjs)
+
+How you can set up Chart.js to work with Svelte. The code accompanies the <a aria-label="Open Rodney Lab post on Svelte Charts" href="https://rodneylab.com/svelte-charts/">post on Svelte Charts</a>. If you have any questions, please drop a comment at the bottom of that page.
+
+## Building and previewing the site
+
+If you're seeing this, you've probably already done this step. Congrats!
+
+```bash
+git clone https://github.com/rodneylab/svelte-chartjs.git
+cd svelte-chartjs
+pnpm install # or npm install
+pnpm run dev
+```
+
+## Building
+
+```bash
+pnpm run build
+```
+
+> You can preview the built app with `pnpm run preview`, regardless of whether you installed an adapter. This should _not_ be used to serve your app in production.
+
+Feel free to jump into the [Rodney Lab matrix chat room](https://matrix.to/#/%23rodney:matrix.org).
---
+++ src/routes/index.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+  import { browser } from '$app/env';
+  import { FY2021 as satisfactionData2021 } from '$lib/data/satisfaction.json';
+  import '@fontsource/merriweather';
+  import { Chart, registerables } from 'chart.js';
+  import { onMount } from 'svelte';
+
+  Chart.register(...registerables);
+
+  let barChartElement: HTMLCanvasElement;
+  let chart: Chart | null = null;
+
+  const chartData = {
+    labels: satisfactionData2021.map(({ framework }) => framework),
+    datasets: [
+      {
+        label: 'Satisfaction (%)',
+        data: satisfactionData2021.map(({ score }) => score),
+        backgroundColor: [
+          'hsl(347 38% 49%)',
+          'hsl(346 65% 63%)',
+          'hsl(346 49% 56%)',
+          'hsl(346 89% 70%)',
+          'hsl(346 90% 76%)',
+          'hsl(346 90% 73%)',
+          'hsl(346 89% 79%)',
+          'hsl(346 89% 85%)',
+          'hsl(347 89% 82%)',
+          'hsl(346 90% 88%)',
+          'hsl(347 87% 94%)',
+          'hsl(347 91% 91%)',
+          'hsl(346 87% 97%)',
+        ],
+        borderColor: ['hsl(43 100% 52%)'],
+        borderRadius: 4,
+        borderWidth: 2,
+      },
+    ],
+  };
+
+  onMount(() => {
+    if (browser) {
+      chart = new Chart(barChartElement, {
+        type: 'bar',
+        data: chartData,
+        plugins: [
+          {
+            id: 'custom_canvas_background_colour',
+            beforeDraw: (chart: Chart) => {
+              const ctx = chart.canvas.getContext('2d');
+              if (ctx) {
+                ctx.save();
+                ctx.globalCompositeOperation = 'destination-over';
+                ctx.fillStyle = 'hsl(13 46% 25%)';
+                ctx.fillRect(0, 0, chart.width, chart.height);
+                ctx.restore();
+              }
+            },
+          },
+        ],
+        options: {
+          plugins: {
+            legend: {
+              display: false,
+            },
+          },
+          scales: {
+            x: {
+              grid: {
+                color: 'hsl(43 100% 52% / 10%)',
+              },
+              ticks: { color: 'hsl(43 100% 52% )' },
+            },
+            y: {
+              beginAtZero: false,
+              ticks: { color: 'hsl(43 100% 52% )', font: { size: 18 } },
+              grid: {
+                color: 'hsl(43 100% 52% / 40%)',
+              },
+              title: {
+                display: true,
+                text: 'Satisfaction (%)',
+                color: 'hsl(43 100% 52% )',
+                font: { size: 24, family: 'Merriweather' },
+              },
+            },
+          },
+        },
+      });
+    }
+  });
+</script>
+
+<main class="main-container">
+  <h1>State of JS 2021 Backend Framework Satisfaction</h1>
+  <section>
+    <canvas bind:this={barChartElement} />
+  </section>
+</main>
+
+<style>
+  :global(html) {
+    background-color: var(--colour-theme);
+    font-family: Merriweather;
+  }
+  :global(h1) {
+    color: var(--colour-dark);
+    font-size: var(--font-size-6);
+  }
+  :global(:root) {
+    --colour-theme: hsl(43 100% 52%); /* selective yellow */
+    --colour-brand: hsl(13 46% 25%); /* irish coffee */
+    --colour-alt: hsl(346 89% 70%); /* froly */
+    --colour-light: hsl(75 100% 98%); /* ceramic */
+    --colour-dark: hsl(182 83% 9%); /* tiber */
+
+    --spacing-12: 3rem;
+    --spacing-18: 4.5rem;
+
+    --max-width-wrapper: 48rem;
+
+    --font-size-6: 3.052rem;
+  }
+  .main-container {
+    width: min(100% - var(--spacing-12), var(--max-width-wrapper));
+    margin: var(--spacing-18) auto;
+  }
+</style>
